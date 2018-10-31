e5675f2d5be1e59d7ec0d1689f3e4b384088a538
rodrigo2711 <37155546+rodrigo2711@users.noreply.github.com>
2018-10-31 20:29:29 -0300

    Create rele.c

---
+++ Biblioteca_projeto/rele.c
@@ -0,0 +1,16 @@
+#include "gpio_sysfs.h"
+#include <unistd.h>
+
+
+int main()
+{
+	int pin=18;
+	if(setGPIO_Out(pin))
+		return -1;
+	if (GPIO_Write(pin,1))
+		return 1;
+	sleep(5);
+	if(unsetGPIO(pin))
+		return 2;
+	return 0;
+}
