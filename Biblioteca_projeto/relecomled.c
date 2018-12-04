a101a7cdd2f99389ed32d42ac0f445e21c6f35d0
rodrigo2711 <37155546+rodrigo2711@users.noreply.github.com>
2018-12-04 21:04:51 -0200

    Update relecomled.c

--- Biblioteca_projeto/relecomled.c
+++ Biblioteca_projeto/relecomled.c
@@ -1,37 +1,32 @@
 #include "gpio_sysfs.h"
 #include <unistd.h>
 
-void fechar(){
 
+int main()
+{
+	int pin1=18;
+	int pin2=23;
+
+	if(setGPIO_Out(pin1))
+		return -1;
+	
+	if (GPIO_Write(pin1,1))
+		return 1;
+	
+	if(setGPIO_Out(pin2))
+		return -1;
+
+	if (GPIO_Write(pin2,1))
+		return 1;
+
+	sleep(10);
+	
 	if(unsetGPIO(pin1))
 		return 2;
 	return 0;
 	if(unsetGPIO(pin2))
 		return 2;
 	return 0;
-	if(unsetGPIO(pin3))
-		return 2;
-	return 0;
-}
-int main()
-{
-	int pin1=18;
-	int pin2=23;
-	int pin3=24;
 
-	if(setGPIO_Out(pin1))
-		return -1;
-	if(setGPIO_Out(pin2))
-		return -1;
-	if(setGPIO_Out(pin3))
-		return -1;
-
-	if (GPIO_Write(pin1,1))
-		return 1;
-	if (GPIO_Write(pin2,1))
-		return 1;
-	if (GPIO_Write(pin3,1))
-		return 1;	
-signal(SIGINT, fechar);
 
 }
