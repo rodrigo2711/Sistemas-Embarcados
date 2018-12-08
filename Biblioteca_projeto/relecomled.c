ce5f1a1dce73ec7c326952799012fe404c0a6970
rodrigo2711 <37155546+rodrigo2711@users.noreply.github.com>
2018-12-07 23:12:04 -0200

    Update relecomled.c

--- Biblioteca_projeto/relecomled.c
+++ Biblioteca_projeto/relecomled.c
@@ -6,24 +6,29 @@ int main()
 {
 	int pin1=18;
 	int pin2=23;
-
+	int pin3=24;
+	
 	if(setGPIO_Out(pin1))
 		return -1;
-	
 	if (GPIO_Write(pin1,1))
 		return 1;
 	
 	if(setGPIO_Out(pin2))
 		return -1;
-
 	if (GPIO_Write(pin2,1))
 		return 1;
-
-	sleep(10);
 	
+	if(setGPIO_Out(pin3))
+		return -1;
+	if (GPIO_Write(pin3,0))
+		return 1;
+	
+	sleep(10);
+		
+	if (GPIO_Write(pin3,1))
+		return 1;
 	if(unsetGPIO(pin1))
 		return 2;
-	return 0;
 	if(unsetGPIO(pin2))
 		return 2;
 	return 0;
